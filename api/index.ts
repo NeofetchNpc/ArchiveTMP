b4b5ad942a5136075b59a8c7e7685acd6658fbe6
YusupKakuu <158158782+NeofetchNpc@users.noreply.github.com>
2024-11-27 05:10:40 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -6,13 +6,13 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   const directoryPath = path.resolve('./uploads');
 
   try {
-    // Filter hanya file gambar
+    // Filter hanya file gambar, video, dan audio
     const files = fs.readdirSync(directoryPath).filter((file) =>
-      /\.(png|jpg|jpeg|mp4|mkv|avi|webm|mov)$/i.test(file)
+      /\.(png|jpg|jpeg|mp4|mkv|avi|webm|mov|mp3|wav|ogg)$/i.test(file)
     );
 
     if (files.length === 0) {
-      return res.status(404).json({ error: 'No images found in the directory.' });
+      return res.status(404).json({ error: 'No images, videos, or audio files found in the directory.' });
     }
 
     // Pilih file secara acak
